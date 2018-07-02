d583010cd80b80913c3d46647ebb0be777035a81
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-07-02 16:21:22 -0700

    Add project files.

---
+++ ConceptArrayPart2/frmNameList.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ConceptArrayPart2
+{
+    partial class frmNameList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSubmitName = new System.Windows.Forms.Button();
+            this.btnDisplayList = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lstNames = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEnterName
+            // 
+            this.lblEnterName.AutoSize = true;
+            this.lblEnterName.Location = new System.Drawing.Point(20, 19);
+            this.lblEnterName.Name = "lblEnterName";
+            this.lblEnterName.Size = new System.Drawing.Size(66, 13);
+            this.lblEnterName.TabIndex = 0;
+            this.lblEnterName.Text = "Enter Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(92, 16);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(249, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnSubmitName
+            // 
+            this.btnSubmitName.Location = new System.Drawing.Point(347, 16);
+            this.btnSubmitName.Name = "btnSubmitName";
+            this.btnSubmitName.Size = new System.Drawing.Size(102, 20);
+            this.btnSubmitName.TabIndex = 2;
+            this.btnSubmitName.Text = "Submit Name";
+            this.btnSubmitName.UseVisualStyleBackColor = true;
+            this.btnSubmitName.Click += new System.EventHandler(this.BtnSubmitName_Click);
+            // 
+            // btnDisplayList
+            // 
+            this.btnDisplayList.Location = new System.Drawing.Point(23, 77);
+            this.btnDisplayList.Name = "btnDisplayList";
+            this.btnDisplayList.Size = new System.Drawing.Size(102, 40);
+            this.btnDisplayList.TabIndex = 3;
+            this.btnDisplayList.Text = "Display List";
+            this.btnDisplayList.UseVisualStyleBackColor = true;
+            this.btnDisplayList.Click += new System.EventHandler(this.BtnDisplayList_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(23, 123);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(102, 35);
+            this.btnSort.TabIndex = 4;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.BtnSort_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(23, 204);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(102, 39);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // lstNames
+            // 
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.Location = new System.Drawing.Point(131, 77);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(318, 173);
+            this.lstNames.TabIndex = 6;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(23, 164);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(102, 34);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(347, 51);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(102, 20);
+            this.btnSearch.TabIndex = 8;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(92, 51);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(249, 20);
+            this.txtSearch.TabIndex = 11;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(23, 57);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(44, 13);
+            this.lblSearch.TabIndex = 12;
+            this.lblSearch.Text = "Search:";
+            // 
+            // frmNameList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 255);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstNames);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnDisplayList);
+            this.Controls.Add(this.btnSubmitName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblEnterName);
+            this.Name = "frmNameList";
+            this.Text = "Name List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSubmitName;
+        private System.Windows.Forms.Button btnDisplayList;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListBox lstNames;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblSearch;
+    }
+}
+
---
+++ ConceptArrayPart2/frmNameList.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConceptArrayPart2
+{
+    public partial class frmNameList : Form
+    {
+        /***************************************************
+         * Create a Windows forms application that allows
+         * the user to type a name in a textbox and add 
+         * them to a user list. The names should be stored 
+         * in an array. When the array gets to 10 elements,
+         * pop up a messagebox telling the user that no
+         * more names may be entered. Finally the user can 
+         * click a display button that will show the SORTED
+         * list of names in REVERSE alphabetical order.  
+         **************************************************/
+        List<string> userNames = new List<string>();
+
+        public frmNameList()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// when Submit Name has been clicked on names
+        /// will be held in memory until Display has been clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSubmitName_Click(object sender, EventArgs e)
+        {
+            if (userNames.Count == 10)
+            {
+                MessageBox.Show("Only 10 usernames are allowed.");
+            }
+
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Please enter a user name.");
+                txtName.Clear();
+                txtName.Focus();
+            }
+            else
+            {
+                userNames.Add(txtName.Text.Trim());
+                MessageBox.Show("User name has been added.");
+            }
+            txtName.Clear();
+            txtName.Focus();
+        }
+
+        /// <summary>
+        /// when DisplayList button is clicked the 
+        /// list of usernames will be populated in 
+        /// the list box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnDisplayList_Click(object sender, EventArgs e)
+        {
+            if (userNames.Count == 0)
+            {
+                MessageBox.Show("Username arraylist is not populated!", "Empty!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string display = string.Join("\n", userNames);
+            MessageBox.Show(display, "Usernames");
+
+        }
+
+        /// <summary>
+        /// when user enters a search critiria a name
+        /// containing that critiria wil populate on the list box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            //list will now show names of users
+            lstNames.Items.Clear();
+
+            string searchTerm = txtSearch.Text.Trim().ToLower();
+
+            //if no search term is not present
+            //exit out of method
+            if (searchTerm == string.Empty)
+            {
+                return;
+            }
+
+            //when search term is present
+            //result should show in list
+            foreach (string uNames in userNames)
+            {
+                
+                if (uNames.ToLower().Contains(searchTerm) || uNames.ToUpper().Contains(searchTerm))
+                {
+                    lstNames.Items.Add(uNames);
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// When user clicks on Sort button
+        /// username list will be sorted in
+        /// alphabetical order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSort_Click(object sender, EventArgs e)
+        {
+            if(userNames != null)
+            {
+                userNames.Reverse();
+            }
+        }
+
+        /// <summary>
+        /// when user click on the Clear button
+        /// name that have been added to list
+        /// will be cleared and a message box
+        /// will confirm that usernames have 
+        /// been cleared
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            userNames.Clear();
+            MessageBox.Show("Usernames have been clear from list!");
+        }
+
+        /// <summary>
+        /// when user click on the close button
+        /// they will be prompt to close the app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            //code when the user clicks on the "Close" button
+            //call a method to close the program
+            CloseApplication();
+        }
+
+        /// <summary>
+        /// when user click on the close button
+        /// BtnClose_Click will call CloseApplication()
+        /// </summary>
+        private void CloseApplication()
+        {
+            //code for closing the program when
+            //the CloseAppication() method is
+            //called from BtnClose()
+            DialogResult result = MessageBox.Show("Are you sure?",
+                                                  "Quit?",
+                                                  MessageBoxButtons.YesNo,
+                                                  MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+
+    }
+}
